0b1b7961ef110979b18432fff0f4262224c68587
BCNGeekMarc <moncusimarc@gmail.com>
2022-10-10 10:29:04 +0200

    reviewed

--- src/js/functions.js
+++ src/js/functions.js
@@ -2,123 +2,123 @@ const minesCounter = document.querySelector('[data-mCounter]')
 const timerCounter = document.querySelector('[data-tCounter]')
 const newGame = document.querySelector('[data-newGame]')
 const gameState = document.querySelector('[data-gameState]')
-const container = document.getElementById("container")
+const container = document.getElementById('container')
 
-var ms = null 
-if(window.location.href.indexOf("testing") > -1){
-  ms = new minesweeper(testingMap);
-}else{
-  ms = new minesweeper();
-}
+var ms = null
+if (window.location.href.indexOf('testing') > -1)  ms = new minesweeper(testingMap)
+else  ms = new minesweeper()
+
 
 window.onload = function () {
-  generateBoard()
-  updateMinesCounter()
+    generateBoard()
+    updateMinesCounter()
 }
 
-function generateBoard(){
-  container.style.setProperty('--grid-rows', ms.rows)
-  container.style.setProperty('--grid-cols', ms.cols)
-  for(let i = 1; i <= ms.rows; i++){
-    for(let j = 1; j <= ms.cols; j++){
-      let cell = document.createElement("div")
-      let cellVal = i+'-'+j
-      container.appendChild(cell).className = "grid-item"
-      cell.setAttribute('id',cellVal)
-      cell.setAttribute('data-testid',cellVal)
-      cellListener(cell,i,j);
-    }
-  }
-}
-
-function updateGame(){
-  updateState()
-  updateMinesCounter()
-  updateBoard()
-}
-
-function updateBoard(){
-  for(let i = 1; i <= ms.rows; i++){
-    for(let j = 1; j <= ms.cols; j++){
-      let boardCell = ms.getCell(i-1,j-1)
-      let cellVal = i+'-'+j
-      let cell = document.getElementById(cellVal)
-      if(!boardCell.opened){
-        switch(boardCell.state){
-          case 'flagged':
-            if(boardCell.content != '*' && ms.getGameState() == 'lost') cell.innerText = 'X'
-            else cell.innerText = '!'
-            break
-          case 'marked':
-            cell.innerText = '?'
-            break
-          case 'none':
-            cell.innerText = ''
-            break
+function generateBoard() {
+    container.style.setProperty('--grid-rows', ms.rows)
+    container.style.setProperty('--grid-cols', ms.cols)
+    for (let i = 1; i <= ms.rows; i++) {
+        for (let j = 1; j <= ms.cols; j++) {
+            let cell = document.createElement('div')
+            let cellVal = i + '-' + j
+            container.appendChild(cell).className = 'grid-item'
+            cell.setAttribute('id', cellVal)
+            cell.setAttribute('data-testid', cellVal)
+            cellListener(cell, i, j)
         }
-      }else{
-        if(boardCell.content == '*' && boardCell.state == 'flagged'){
-          cell.innerText = '!'
-        }
-        else cell.innerText = boardCell.content
-      }
     }
-  }
-}
-function updateMinesCounter(){
-  minesCounter.innerText = ms.getMines();
 }
 
-function nextCellState(cell,row,col){
-  switch(cell.innerText){
-    case '!':
-      ms.markCell(row-1,col-1)
-      break;
-    case '':
-      ms.flagCell(row-1,col-1)
-      break;
-    case '?':
-      ms.noStateCell(row-1,col-1)
-      break;
-  }
-}
-function updateState(){
-  switch(ms.getGameState()){
-    case 'lost':
-      gameState.innerText = 'Game Lost';
-      break;
-    case 'won':
-      gameState.innerText = 'Game Won';
-      break;
-    default:
-      gameState.innerText = '';
-      break;
-  }
+function updateGame() {
+    updateState()
+    updateMinesCounter()
+    updateBoard()
 }
 
-function cellListener(cell, row, col){
-  cell.addEventListener('contextmenu', e=> e.preventDefault())
-  cell.addEventListener('mousedown',function(event){
-    if(!ms.getCell(row-1,col-1).opened){
-    switch(event.which){
-        case 1:
-          ms.openCell(row-1,col-1)
-          break
-        case 3:
-          nextCellState(cell,row,col)
-          break
+function updateBoard() {
+    for (let i = 1; i <= ms.rows; i++) {
+        for (let j = 1; j <= ms.cols; j++) {
+            let boardCell = ms.getCell(i - 1, j - 1)
+            let cellVal = i + '-' + j
+            let cell = document.getElementById(cellVal)
+            if (!boardCell.opened) {
+                switch (boardCell.state) {
+                    case 'flagged':
+                        if (
+                            boardCell.content != '*' &&
+                            ms.getGameState() == 'lost'
+                        )
+                            cell.innerText = 'X'
+                        else cell.innerText = '!'
+                        break
+                    case 'marked':
+                        cell.innerText = '?'
+                        break
+                    case 'none':
+                        cell.innerText = ''
+                        break
+                }
+            } else {
+                if (boardCell.content == '*' && boardCell.state == 'flagged') cell.innerText = '!'
+                else cell.innerText = boardCell.content
+            }
+        }
+    }
+}
+
+function updateMinesCounter() {
+    minesCounter.innerText = ms.getMines()
+}
+
+function nextCellState(cell, row, col) {
+    switch (cell.innerText) {
+        case '!':
+            ms.markCell(row - 1, col - 1)
+            break
+        case '':
+            ms.flagCell(row - 1, col - 1)
+            break
+        case '?':
+            ms.noStateCell(row - 1, col - 1)
+            break
+    }
+}
+
+function updateState() {
+    switch (ms.getGameState()) {
+        case 'lost':
+            gameState.innerText = 'Game Lost'
+            break
+        case 'won':
+            gameState.innerText = 'Game Won'
+            break
         default:
-          break
+            gameState.innerText = ''
+            break
     }
-    updateGame()
-  }
-  });
 }
-newGame.addEventListener('click', () =>{
-  if(window.location.href.indexOf("testing") > -1){
-    ms = new minesweeper(testingMap);
-  }else{
-    ms = new minesweeper();
-  }
-  updateGame()
+
+function cellListener(cell, row, col) {
+    cell.addEventListener('contextmenu', (e) => e.preventDefault())
+    cell.addEventListener('mousedown', function (event) {
+        if (!ms.getCell(row - 1, col - 1).opened) {
+            switch (event.which) {
+                case 1:
+                    ms.openCell(row - 1, col - 1)
+                    break
+                case 3:
+                    nextCellState(cell, row, col)
+                    break
+                default:
+                    break
+            }
+            updateGame()
+        }
+    })
+}
+
+newGame.addEventListener('click', () => {
+    if (window.location.href.indexOf('testing') > -1) ms = new minesweeper(testingMap)
+    else  ms = new minesweeper()
+    updateGame()
 })
--- src/js/minesweeper.js
+++ src/js/minesweeper.js
@@ -1,152 +1,134 @@
 const defaultCell = {
     opened: false,
     state: 'none',
-    content: '.'
+    content: '.',
 }
 
 class minesweeper {
-    constructor(map=this.mapCreate()){
-        this.board = JSON.parse(JSON.stringify(map[1])); 
+    constructor(map = this.mapCreate()) {
+        this.board = JSON.parse(JSON.stringify(map[1]))
         this.rows = map[0][0]
         this.cols = map[0][1]
         this.mines = map[0][2]
-        this.gameState = 'notFinished'
+        this.gameState = 'none'
     }
-    getMines(){
+    getMines() {
         return this.mines
     }
-    getBoard(){
+    getBoard() {
         return this.board
     }
-    getCell(row,col){
+    getCell(row, col) {
         return this.board[row][col]
     }
-    getGameState(){
+    getGameState() {
         return this.gameState
     }
 
-    mapCreate(rows=8,cols=8,mines=10){
+    mapCreate(rows = 8, cols = 8, mines = 10) {
         let mRow, mCol
         var board = new Array(rows)
-        for(let i = 0; i<rows; i++){
+        for (let i = 0; i < rows; i++) {
             board[i] = []
-            for (let j = 0; j <cols; j++) {
-                board[i].push({...defaultCell})
+            for (let j = 0; j < cols; j++) {
+                board[i].push({ ...defaultCell })
             }
         }
         for (let i = 0; i < mines; i++) {
-            do{
-            mRow = Math.floor(Math.random() * rows)
-            mCol = Math.floor(Math.random() * cols)
-            }while(board[mRow][mCol].content == '*')
+            do {
+                mRow = Math.floor(Math.random() * rows)
+                mCol = Math.floor(Math.random() * cols)
+            } while (board[mRow][mCol].content == '*')
             board[mRow][mCol].content = '*'
         }
-        return [[cols,rows,mines],board]
+        return [[cols, rows, mines], board]
     }
 
-    flagCell(row,col){
+    flagCell(row, col) {
         this.board[row][col].state = 'flagged'
         this.calculateMines()
     }
 
-    markCell(row,col){
+    markCell(row, col) {
         this.board[row][col].state = 'marked'
         this.calculateMines()
     }
 
-    noStateCell(row,col){
+    noStateCell(row, col) {
         this.board[row][col].state = 'none'
-        this.calculateMines();
+        this.calculateMines()
     }
 
-    calculateMines(){
-        let tempMines=0
-        this.board.forEach(row => {
-            row.forEach(cell =>{
-                if(cell.content == '*'){
-                    tempMines++;
-                }
-                if(cell.state == 'flagged'){
-                    tempMines--;
-                }
+    calculateMines() {
+        let tempMines = 0
+        this.board.forEach((row) => {
+            row.forEach((cell) => {
+                if (cell.content == '*') tempMines++
+                if (cell.state == 'flagged') tempMines--
             })
         })
-        this.mines = tempMines;
+        this.mines = tempMines
     }
 
-    openCell(row,col){
+    openCell(row, col) {
         this.board[row][col].opened = true
-        switch(this.board[row][col].content){
+        switch (this.board[row][col].content) {
             case '*':
                 this.openAllMines()
                 this.board[row][col].state = 'none'
                 break
             case '.':
-                this.board[row][col].content = this.countAdjCells(row,col)
-                if(this.board[row][col].content==0){
-                    this.openAdjacentCells(row,col)
-                }
-                this.noStateCell(row,col)
-                
+                this.board[row][col].content = this.countAdjCells(row, col)
+                if (this.board[row][col].content == 0)
+                    this.openAdjacentCells(row, col)
+                this.noStateCell(row, col)
                 break
-            }    
-        this.gameState = this.checkGameState();
+        }
+        this.gameState = this.checkGameState()
     }
 
-    openAdjacentCells(row,col){
-        for(let i = row-1; i<=row+1; i++){
-            for (let j = col-1; j <=col+1; j++) {
-                if(i<0 || i>=this.rows){
-                    continue}
-                if(j<0 || j>=this.cols){
-                    continue}
-                if(this.board[i][j].opened){
-                    continue}
-                if(this.board[i][j].state == 'flagged'){
-                    continue}
-                this.openCell(i,j);
+    openAdjacentCells(row, col) {
+        for (let i = row - 1; i <= row + 1; i++) {
+            for (let j = col - 1; j <= col + 1; j++) {
+                if (i < 0 || i >= this.rows) continue
+                if (j < 0 || j >= this.cols) continue
+                if (this.board[i][j].opened) continue
+                if (this.board[i][j].state == 'flagged') continue
+                this.openCell(i, j)
             }
         }
     }
 
-    countAdjCells(row,col){
-        let value=0;
-        for(let i = row-1; i<=row+1; i++){
-            for (let j = col-1; j <=col+1; j++) {
-                if(i<0 || i>=this.rows){continue}
-                if(j<0 || j>=this.cols){continue}
-                if(this.board[i][j].content=='*'){
-                    value++;
-                }
+    countAdjCells(row, col) {
+        let value = 0
+        for (let i = row - 1; i <= row + 1; i++) {
+            for (let j = col - 1; j <= col + 1; j++) {
+                if (i < 0 || i >= this.rows) continue
+                if (j < 0 || j >= this.cols) continue
+                if (this.board[i][j].content == '*') value++
             }
         }
-        return value;
+        return value
     }
 
-    openAllMines(){
-        for(let i = 0; i < this.rows; i++){
+    openAllMines() {
+        for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
-                if(this.board[i][j].opened){
-                    continue}
-                if(this.board[i][j].content == '*'){
+                if (this.board[i][j].opened) continue
+                if (this.board[i][j].content == '*')
                     this.board[i][j].opened = true
-                }
             }
         }
     }
 
-    checkGameState(){
-        for(let i = 0; i < this.rows; i++){
+    checkGameState() {
+        for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
-                if(!this.board[i][j].opened){
-                    if(this.board[i][j].content != '*'){
-                        return 'none'
-                    }
+                if (!this.board[i][j].opened) {
+                    if (this.board[i][j].content != '*') return 'none'
                 }
-                if(this.board[i][j].opened){
-                    if(this.board[i][j].content == '*'){
-                        return 'lost'
-                    }
+                if (this.board[i][j].opened) {
+                    if (this.board[i][j].content == '*') return 'lost'
                 }
             }
         }
